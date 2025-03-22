adda24afc0be1173bde8578992ae2738a589d200
holos <holosnikitatt@gmail.com>
2025-03-22 07:33:50 +0200

    Update and rename main.asm to kernel.asm

---
+++ src/kernel.asm
@@ -0,0 +1,30 @@
+section .multiboot
+align 4
+
+; Multiboot 1 header
+multiboot_header:
+    dd 0x1BADB002              ; Magic number (Multiboot 1)
+    dd 0x00000003               ; Flags (align modules on page boundaries + provide memory map)
+    dd -(0x1BADB002 + 0x00000003) ; Checksum
+
+section .text
+global _start
+
+_start:
+    ; Загрузчик передаёт управление сюда
+    cli                         
+    mov esp, stack_top          
+
+    ; Вызов функции на C
+    extern kernel_main
+    call kernel_main
+
+    ; Бесконечный цикл (на случай, если kernel_main вернётся)
+    hlt
+    jmp $
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384                  
+stack_top:
