38ae7a696e72918ec1fcc6068a322e9d60a70197
holos <holosnikitatt@gmail.com>
2025-03-22 11:12:23 +0200

    Update kernel.asm

--- src/kernel.asm
+++ src/kernel.asm
@@ -4,24 +4,27 @@ align 4
 ; Multiboot 1 header
 multiboot_header:
     dd 0x1BADB002              ; Magic number (Multiboot 1)
-    dd 0x00000003               ; Flags (align modules on page boundaries + provide memory map)
+    dd 0x00000003              ; Flags (align modules on page boundaries + provide memory map)
     dd -(0x1BADB002 + 0x00000003) ; Checksum
 
 section .text
+bits 64                       
 global _start
 
 _start:
-    cli                         
-    mov esp, stack_top          
+    cli                       
+    mov rsp, stack_top        
 
+    ; Вызов функции на C
     extern kernel_main
     call kernel_main
 
+    ; Бесконечный цикл (на случай, если kernel_main вернётся)
     hlt
     jmp $
 
 section .bss
 align 16
 stack_bottom:
-    resb 16384                  
+    resb 16384                
 stack_top:
